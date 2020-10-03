e06020f6cb6de0f437b048f32afe988964c69c78
Orlando G Morales <57885081+OGMorales17@users.noreply.github.com>
2020-10-03 12:24:57 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ document.getElementsByClassName('second');
 document.querySelector('ol', 'third');
 // Give the section with an id of container the text “Hello!”.
 const container = document.querySelector('#container');
-container.innerText = `Hello!`;
+container.prepend = `Hello!`;
 // Add the class main to the div with a class of footer.
 const footer1 = document.querySelector('.footer');
 footer1.className += ` main`;
@@ -16,12 +16,12 @@ footer1.className += ` main`;
 const footer2 = document.querySelector('.footer');
 footer2.classList.remove('main');
 // Create a new li element.
-const newLi = document.createElement('li');
+const newLi = document.createElement('li'); //const newLi = document.createTextNode('li');
 // Give the li the text “four”.
 newLi.innerText = 'four';
 // Append the li to the ul element.
 const ul = document.querySelector('ul');
-ul.append(newLi) // this code throw an error: Cannot read property 'appendChild' of null, I have try to fix the problem including jquery, but still not working
+ul.appendChild(newLi) // this code throw an error: Cannot read property 'appendChild' of null, I have try to fix the problem including jquery, but still not working
     // Loop over all of the lis inside the ol tag and give them a background color of “green”.
 const liInOl = document.querySelectorAll('ol li');
 for (let i = 0; i < liInOl.length; i++) {
